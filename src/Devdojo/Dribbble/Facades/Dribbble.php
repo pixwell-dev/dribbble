b0e94368b5e758a3dfb19d4d2afc7f7996d40fcd
Daniel Janiga <danieljaniga@gmail.com>
2016-08-20 11:49:09 +0200

    fixed facadeAccessor

--- src/Devdojo/Dribbble/Facades/Dribbble.php
+++ src/Devdojo/Dribbble/Facades/Dribbble.php
@@ -1,14 +1,18 @@
 <?php namespace Devdojo\Dribbble\Facades;
- 
+
 use Illuminate\Support\Facades\Facade;
- 
-class Dribbble extends Facade {
- 
-  /**
-   * Get the registered name of the component.
-   *
-   * @return string
-   */
-  protected static function getFacadeAccessor() { return 'dribbble'; }
- 
+
+class Dribbble extends Facade
+{
+    
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return \Devdojo\Dribbble\Dribbble::class;
+    }
+    
 }
